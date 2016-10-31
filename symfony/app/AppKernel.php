2e49d8b81a79be6ef296bb9f21c32f46525385a6
Riccardo Ravaro <riccardo@MAC0030.local>
2016-10-31 14:38:41 +0000

    bootstrap, new route configuration

--- symfony/app/AppKernel.php
+++ symfony/app/AppKernel.php
@@ -5,15 +5,9 @@ use Symfony\Component\Config\Loader\LoaderInterface;
 
 class AppKernel extends Kernel
 {
-    public function __construct($environment, $debug){
-         date_default_timezone_set( 'Europe/Paris' );
-         parent::__construct($environment, $debug);
-    }
-
-    
     public function registerBundles()
     {
-        $bundles = [
+        $bundles = array(
             new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
             new Symfony\Bundle\SecurityBundle\SecurityBundle(),
             new Symfony\Bundle\TwigBundle\TwigBundle(),
@@ -23,10 +17,10 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new CarBundle\CarBundle(),
-            new Knp\Bundle\MenuBundle\KnpMenuBundle()
-        ];
+            new \Knp\Bundle\MenuBundle\KnpMenuBundle()
+        );
 
-        if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
+        if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
             $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
--- symfony/src/AppBundle/Menu/Builder.php
+++ symfony/src/AppBundle/Menu/Builder.php
@@ -11,7 +11,9 @@ class Builder
     public function mainMenu(MenuFactory $factory, array $options)
     {
         $menu = $factory->createItem('root');
+        $menu->setChildrenAttribute('class', 'nav navbar-nav');
         $menu->addChild('Home', ['route' => 'homepage']);
+        $menu->addChild('Offer', ['route' => 'offer']);
         return $menu;
     }
 
