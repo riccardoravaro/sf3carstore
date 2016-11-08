fa2431585f151fb4d7fd33722e1cd789cbde1e85
riccardoravaro <ravaro.riccardo@gmail.com>
2016-11-08 16:54:03 +0000

    phpunit implementation

--- symfony/app/AppKernel.php
+++ symfony/app/AppKernel.php
@@ -5,6 +5,14 @@ use Symfony\Component\Config\Loader\LoaderInterface;
 
 class AppKernel extends Kernel
 {
+
+    public function __construct($environment, $debug)
+    {
+        date_default_timezone_set('Europe/London');
+        parent::__construct($environment, $debug);
+    }
+
+
     public function registerBundles()
     {
         $bundles = array(
@@ -25,6 +33,7 @@ class AppKernel extends Kernel
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
+            $bundles[] = new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle();
         }
 
         return $bundles;
--- symfony/tests/AppBundle/Controller/DefaultControllerTest.php
+++ symfony/tests/AppBundle/Controller/DefaultControllerTest.php
@@ -13,6 +13,19 @@ class DefaultControllerTest extends WebTestCase
         $crawler = $client->request('GET', '/');
 
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        $this->assertContains('Welcome to Symfony', $crawler->filter('#container h1')->text());
+        $this->assertContains('Welcome to AutoTrader', $crawler->filter('h1')->text());
+    }
+
+    public function testOffer() {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/our-cars');
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertContains('Our offer', $crawler->filter('h1')->text());
+
+        $link = $crawler->filter('a:contains("Show details")')->eq(0)->link();
+        $crawler = $client->click($link);
+        $this->assertContains('BMW X1', $crawler->filter('h1')->text());
     }
 }
